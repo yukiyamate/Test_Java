43e060bb74d9fbdff8750efcdb2c0753a2c377a3
yukiyamate <yuki.yamate@metateam.co.jp>
2024-05-02 10:53:24 +0900

    配列基礎修正

--- Array/Main.java
+++ Array/Main.java
@@ -3,7 +3,7 @@ public class Main {
         int[] scores = {20, 30, 45, 35};
 
         for (int i = 0; i < scores.length; i++) {
-            System.out.println("番目の社員の点数は" + scores[i] + "です。");
+            System.out.println((i+1) + "番目の社員の点数は" + scores[i] + "です。");
         }
 
         System.out.println("過去問を解いた社員は全員で " + scores.length + "人です。");
