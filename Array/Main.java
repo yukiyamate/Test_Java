99f83aa84617323409a442872757113567418597
yukiyamate <yuki.yamate@metateam.co.jp>
2024-05-02 10:21:57 +0900

    配列基礎

---
+++ Array/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] scores = {20, 30, 45, 35};
+
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println("番目の社員の点数は" + scores[i] + "です。");
+        }
+
+        System.out.println("過去問を解いた社員は全員で " + scores.length + "人です。");
+    }
+}
